c922198d5c9fe47701265cea2a0fbd64cf800af1
ravijabade12 <ravisjabade12@gmail.com>
2026-06-28 12:06:42 +0530

    feat: add user and video models, implement JWT and bcrypt for authentication, and update package dependencies

--- src/utils/aysncHandler.js
+++ src/utils/aysncHandler.js
@@ -1,8 +1,5 @@
-const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
-};
+const asyncHandler = (requestHandler) => (req, res, next) =>
+  Promise.resolve(requestHandler(req, res, next)).catch(next);
 
 export { asyncHandler };
 
